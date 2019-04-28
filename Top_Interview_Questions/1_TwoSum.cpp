4a74a8543409527b1f38b613b5781affe97e0f49
JingyuTongdlut <cptbtptp@mail.dlut.edu.cn>
2019-04-28 16:31:24 +0800

    creat topinterview dirctory

---
+++ Top_Interview_Questions/1_TwoSum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        // vector<int> ans;
+        unordered_map<int, int> mem;
+        for(int i = 0; i < nums.size(); ++i) {
+            mem[nums[i]] = i;
+        }
+        
+        for(int i = 0; i < nums.size(); ++i) {
+            if(mem.count(target - nums[i]) && mem[target - nums[i]] != i)
+                return {i, mem[target - nums[i]]};
+        }
+        
+        return {};
+    }
+};
